12a7fb692ca6a15fdd7bedd1d9ddd0625c92b7cf
shenmemingzine <rongwingyu@gmail.com>
2019-02-01 14:50:22 -0800

    Create 6. ZigZag Conversion.py

---
+++ 6. ZigZag Conversion.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        
+        s_len = len(s)
+        if numRows == 1 or s_len <= numRows:
+            return s
+        
+        zig_zag = [""]*numRows
+        
+        for idx in range(s_len):
+            temp_pos = idx % (2*numRows - 2)
+            if temp_pos < numRows:
+                zig_zag[temp_pos] += s[idx]
+            else:
+                zig_zag[2*numRows - 2 - temp_pos] += s[idx]
+        
+        return "".join(zig_zag)
