251fe4ae9737dd58f72dad0fbef926c1d82d3264
shenmemingzine <rongwingyu@gmail.com>
2019-06-16 16:47:44 -0700

    Update 287. Find the Duplicate Number.py

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -14,3 +14,25 @@ class Solution(object):
                 return n
             seen.add(n)
             
+
+"""
+cycle detection
+"""
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        meet = slow = fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                while meet != slow:
+                    meet = nums[meet]
+                    slow = nums[slow]
+                    
+                return meet
+            
+            
