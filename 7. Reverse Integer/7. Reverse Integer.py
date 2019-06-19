1d3905529838b29d792ca6a5d0ec6bb21d9f18ed
shenmemingzine <rongwingyu@gmail.com>
2019-06-19 16:01:13 -0700

    Update 7. Reverse Integer.py

--- 7. Reverse Integer/7. Reverse Integer.py
+++ 7. Reverse Integer/7. Reverse Integer.py
@@ -18,3 +18,31 @@ class Solution:
         if -2**31 <= x <= 2**31-1:
             return x
         return 0
+
+"""
+better solution
+"""
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        threshold = 2**31
+        res = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        while x != 0:
+            carried = x%10
+            x //= 10
+            
+            if (sign*res > threshold/10) or (sign*res == threshold/10 and carried > 7):
+                return 0
+            elif (sign*res < -threshold/10) or (sign*res == -threshold/10 and carried < -8):
+                return 0
+            res = res*10 + carried
+
+        return sign*res
+        
+        
