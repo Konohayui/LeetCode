5bbebfc719dd5518b2e79be89f40536150ed98a1
shenmemingzine <rongwingyu@gmail.com>
2019-06-19 14:14:27 -0700

    Update and rename 5. Longest Palindromic Substring.py to 5. Longest Palindromic Substring/5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring/5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring/5. Longest Palindromic Substring.py
@@ -27,3 +27,30 @@ class Solution:
             
             
             
+"""
+better solution:
+search from center
+"""
+
+class Solution:
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+            
+        return s[l+1:r]
+    
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            temp = self.helper(s, i, i) # case cabad, odd
+            if len(temp) > len(res):
+                res = temp
+                
+            temp = self.helper(s, i, i+1) # case cabbad, even
+            if len(temp) > len(res):
+                res = temp
+                
+        return res
+        
+    
