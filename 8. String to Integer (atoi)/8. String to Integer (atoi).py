f793527f232b40070ab87a007df7a7667a75c048
shenmemingzine <rongwingyu@gmail.com>
2019-06-20 15:58:38 -0700

    Update and rename 8. String to Integer (atoi).py to 8. String to Integer (atoi)/8. String to Integer (atoi).py

--- 8. String to Integer (atoi)/8. String to Integer (atoi).py
+++ 8. String to Integer (atoi)/8. String to Integer (atoi).py
@@ -30,4 +30,38 @@ class Solution:
         return num
         
             
+"""
+better solution
+"""
+class Solution:
+    def myAtoi(self, s: 'str') -> 'int':
+        if not str:
+            return 0
+        
+        s = s.strip()
+        if len(s) == 0:
+            return 0
+        
+        sign = 1
+        idx = 0
+        if s[0] == "-":
+            sign = -1
+            idx = 1
+        elif s[0] == "+":
+            sign = sign
+            idx = 1
+        
+        res = ""
+        for n in s[idx:]:
+            if n.isdigit():
+                res += n
+            else:
+                break
+                
+        num = sign*int(res) if len(res) != 0 else 0
+        if num > 2**31 - 1:
+            return 2**31 - 1
+        elif num < -2**31:
+            return -2**31
+        return num
         
