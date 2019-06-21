a1daed55b2c6c0897bc3e1e1ea44eaf98f2e0738
shenmemingzine <rongwingyu@gmail.com>
2019-06-21 14:32:19 -0700

    Update and rename 4. Median of Two Sorted Arrays.py to 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.py
@@ -1,3 +1,33 @@
+"""
+O(log(m+n))
+"""
+class Solution:
+    def getKth(self, nums1, nums2, k):
+        len1, len2 = len(nums1), len(nums2) # assume nums1 is the shortest
+        if len1 > len2:
+            return self.getKth(nums2, nums1, k)
+        if len1 == 0:
+            return nums2[k - 1]
+        if k == 1:
+            return min(nums1[0], nums2[0])
+        pos1 = min(k//2, len1)
+        pos2 = k - pos1
+        
+        if nums1[pos1 - 1] < nums2[pos2 - 1]:
+            return self.getKth(nums1[pos1:], nums2, pos2)
+        else:
+            return self.getKth(nums1, nums2[pos2:], pos1)
+        
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len1, len2 = len(nums1), len(nums2)
+        if (len1 + len2) % 2 == 1:
+            return self.getKth(nums1, nums2, (len1 + len2)//2 + 1)
+        return (self.getKth(nums1, nums2, (len1 + len2)//2) + self.getKth(nums1, nums2, (len1 + len2)//2 + 1))*0.5
+    
+
+"""
+O(m+n)
+"""
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
         """
