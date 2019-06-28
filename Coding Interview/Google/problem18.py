180d6ff75875502ff141d0be9701762cd1ed0b50
shenmemingzine <rongwingyu@gmail.com>
2019-06-28 14:41:16 -0700

    Update problem18.py

--- Coding Interview/Google/problem18.py
+++ Coding Interview/Google/problem18.py
@@ -9,3 +9,32 @@ For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7,
 * 8 = max(7, 8, 7)
 """
 
+"""
+O(n)
+double-ended queue
+"""
+
+def find_max(array):
+  # windows will record elements' indexes
+  res, windows = [], []
+  
+  for i, n in enumerate(array):
+    # keep/make sure the rightmost element is the smallest
+    while windows and array[windows[-1]] <= n:
+      windows.pop()
+    
+    # add new element
+    windows.append(i)
+    
+    # once the size of windows is larger 
+    # than k, remove the leftmost element
+    if i - windows[0] >= k:
+      windows.pop(0)
+    
+    # add the maximum element in the windows
+    if i + 1 >= k:
+      res.append(array[windows[0]])
+      
+  return res
+  
+  
