422dc217e5b12e99fff5bad89f897c862bcb1608
shenmemingzine <rongwingyu@gmail.com>
2019-07-21 19:36:07 -0700

    Update problem 34.py

--- Coding Interview/Quora/problem 34.py
+++ Coding Interview/Quora/problem 34.py
@@ -12,4 +12,25 @@ but "ecarace" comes first alphabetically.
 As another example, given the string "google", you should return "elgoogle".
 """
 
+def min_insertion_pal(s):
+    if s == s[::-1]:
+        return s
+        
+    if s[0] == s[-1]:
+        return s[0] + min_insertion_pal(s[1:-1]) + s[0]
+    else:
+        str1 = s[0] + min_insertion_pal(s[1:]) + s[0]
+        str2 = s[-1] + min_insertion_pal(s[:-1]) + s[-1]
+        
+        if len(str1) < len(str2):
+            return str1
+        elif len(str1) > len(str2):
+            return str2 
+        else:
+            return str1 if str1 < str2 else str2
+            
+assert min_insertion_pal("race") == "ecarace"
+assert min_insertion_pal("google") == "elgoogle"
+assert min_insertion_pal("banana") == "bananab"
+assert min_insertion_pal("ace") == "acea"
 
